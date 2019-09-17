08c095c0b69eb2378dd4e795bf99e2056d635cf6
Sarah <paynesa@sas.upenn.edu>
2019-09-16 21:42:29 -0400

    co-occurrence

--- co-occurrence_embeddings.py
+++ co-occurrence_embeddings.py
@@ -2,12 +2,21 @@ import json, spacy, string, os, operator
 import pandas as pd
 import numpy as np
 p = string.punctuation
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_lg")
+from parseargs import ArgumentParser
+
+#parse the arguments from the command line
+def parse_args():
+	parser = ArgumentParser()
+	parser.add_argument("--i", default=None, type=str, help = "the directory containing the unprocessed BNC files")
+	parser.add_argument("--o", default=None, type=str, help = "the file where you would like your embeddings to be written ")
+	return args
 
 def vocab():
+	args = parse_args()
 	d = {}
-	for i in os.listdir('BNC'):
-		for line in open('BNC/'+i, 'r'):
+	for i in os.listdir(args.i):
+		for line in open(args.i+i, 'r'):
 			doc = line.split()
 			for word in doc:
 				if word in p or '\n' in word or word.isdigit():
@@ -30,13 +39,14 @@ def vocab():
 	return()
 
 def tcm():
+	args = parse_args()
 	finaldict = {}
 	d = {}
 	for line in open('vocab.txt', 'r'):
 		d[line.strip()] = 0
 	print(d)
-	for file in os.listdir('BNC'):
-		for line in open('BNC/'+file, 'r'):
+	for file in os.listdir(args.i):
+		for line in open(args.i+file, 'r'):
 			line = line.split()
 			for i in range(len(line)-1):
 				if line[i] not in finaldict:
@@ -45,15 +55,12 @@ def tcm():
 				x = max(0, i-5)
 				y = min(i+5, len(line)-1)
 				for k in range (x, y+1):
-					#print(line[k])
-					#print(finaldict[line[i]])
 					if line[k] in d:
 						finaldict[line[i]][line[k]] +=1
-						#print(finaldict)
 		print("Done with {}".format(file))
 	print("Writing embeddings...")
 	for k, v in finaldict.items():
-		with open('/data1/sarah/l4k_embeddings.txt' , 'a') as f:
+		with open(args.o , 'a') as f:
 			f.write(k+" ")
 			x = np.asarray(list(v.values()))
 			np.savetxt(f, x.reshape(1, x.shape[0]))
